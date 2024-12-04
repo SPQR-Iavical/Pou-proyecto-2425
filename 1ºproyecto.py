e9ccd698975c5207ed59c74ec07be01f81a09568
SPQR-Iavical <iagovinan@gmail.com>
2024-12-04 19:09:28 +0100

    comienzo

---
+++ 1ºproyecto.py
@@ -0,0 +1,10 @@
+N = input("cual numero es el SCP: ")
+
+if (N == "096"):
+    print("muy bien")
+
+elif(N > "096"):
+    print("alto, vuelve a intentatlo")
+
+elif(N < "096"):
+    print("bajo, vuelve a intentarlo")
