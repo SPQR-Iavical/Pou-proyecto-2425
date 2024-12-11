ef334a5377e90d78cc8009cb5fe661d8dd316d9d
SPQR-Iavical <iagovinan@gmail.com>
2024-12-11 19:03:02 +0100

    Update 1ºproyecto.py

--- 1ºproyecto.py
+++ 1ºproyecto.py
@@ -1,11 +1,32 @@
-N = input("cual numero es el SCP: ")
+A = int(input("Nivel de Fuerza: "))
+B = input("Clase 1,2 Ó 3: ")
+Primaria = "primaria"
+Secundaria = "secundaria"
+Tercera = "tercera"
 
 
-if (N == "096"):
-    print("muy bien")
+if (A >= 1 and A <= 5):
 
-elif(N > "096"):
-    print("alto, vuelve a intentatlo")
+    if (A == "1"): 
+         print("es un objeto")
+        
+    elif(A == "2"):
+        print("es debil")
 
-elif(N < "096"):
-    print("bajo, vuelve a intentarlo")
+    elif(A == "3"):
+        print("fuerte")
+
+    elif(A == "4"):
+         print("bastante fuerte")
+    if (B  == 1):
+         print("es el SCP-5372")
+    elif(B == 2):
+        print("es el SCP-4967")
+    elif(B == 3):
+        print("el SCP-650-KO")
+
+elif(A <= 6):
+    print("muy alto, vuelve a intentarlo")
+
+elif(A >= 0):
+    print("muy bajo vuelve a intentarlo")
